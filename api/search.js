eee6c7ec37bc5a5e61482bf303a15d84d0e6ba9f
r0oting <rooting2ml@gmail.com>
2026-05-25 19:04:10 +0900

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,24 +1,35 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+
     // CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // preflight 처리
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
     const query = req.query.query;
 
     const ALADDIN_TTB_KEY = process.env.ALADDIN_TTB_KEY;
 
     try {
-        const url = `https://www.aladdin.co.kr/ttb/api/ItemSearch.aspx?TTBKey=${ALADDIN_TTB_KEY}&Query=${encodeURIComponent(query)}&QueryType=Title&MaxResults=5&start=1&SearchTarget=Book&output=js&Version=20131101`;
+
+        const url =
+            `https://www.aladdin.co.kr/ttb/api/ItemSearch.aspx?TTBKey=${ALADDIN_TTB_KEY}&Query=${encodeURIComponent(query)}&QueryType=Title&MaxResults=5&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
         const response = await axios.get(url);
 
-        res.status(200).json(response.data);
+        return res.status(200).json(response.data);
 
     } catch (error) {
+
         console.error(error);
 
-        res.status(500).json({
+        return res.status(500).json({
             error: error.message
         });
     }
